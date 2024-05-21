ff5a0b33b4c88bb5018f17e360c3faa2001b5df4
yeonju52 <yeonju509@gmail.com>
2024-05-21 12:15:41 +0900

    change from srv to client

--- human_follower/src/dynamic_follow.cpp
+++ human_follower/src/dynamic_follow.cpp
@@ -17,51 +17,72 @@ struct HumanPose {
 geometry_msgs::msg::Twist velOutput;
 
 HumanPose p;
-const double target_distance = 1.0; // 목표 거리 (예: 1미터)
+// const double target_distance = 1.0; // 목표 거리 (예: 1미터)
 
 class HumanFollower : public rclcpp::Node {
 public:
     HumanFollower() : Node("human_follower") {
-        // QoS Profile 설정
-        rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
-        custom_qos_profile.history = RMW_QOS_POLICY_HISTORY_KEEP_LAST;
-        custom_qos_profile.depth = 10;
-        custom_qos_profile.reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
+        // // QoS Profile 설정
+        // rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
+        // custom_qos_profile.history = RMW_QOS_POLICY_HISTORY_KEEP_LAST;
+        // custom_qos_profile.depth = 10;
+        // custom_qos_profile.reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
 
         RCLCPP_INFO(this->get_logger(), "Initialized node");
-        service_server_ = this->create_service<yolov8_msgs::srv::TargetPose>(
-            "yolo/target_pose", std::bind(&HumanFollower::handle_request, this, std::placeholders::_1, std::placeholders::_2), custom_qos_profile);
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        srv_client = this->create_client<yolov8_msgs::srv::TargetPose>("yolo/target_pose"); 
+        // , std::bind(&HumanFollower::handle_request, this, std::placeholders::_1, std::placeholders::_2), custom_qos_profile);
+        pub = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(500), std::bind(&HumanFollower::check_and_request, this));
     }
 
 private:
-    void handle_request(
-        const std::shared_ptr<yolov8_msgs::srv::TargetPose::Request> request,
-        std::shared_ptr<yolov8_msgs::srv::TargetPose::Response> response) {
+    void check_and_request() {
+        if (!p.valid) {
+            request_target();
+        }
+    }
 
+    void request_target() {
+        auto request = std::make_shared<yolov8_msgs::srv::TargetPose::Request>();
+        // std::shared_ptr<yolov8_msgs::srv::TargetPose::Response> response;
+        while (!srv_client->wait_for_service(std::chrono::seconds(1))) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+                return;
+            }
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service not available, waiting again...");
+        }
+
+        auto result = srv_client->async_send_request(request);
+        result.wait();
+        try {
+            auto response = result.get();
+            handle_response(response);
+        } catch (const std::exception &e) {
+            RCLCPP_ERROR(this->get_logger(), "Service call failed: %s", e.what());
+        }
+    }
+
+    void handle_response(
+        const std::shared_ptr<yolov8_msgs::srv::TargetPose::Response> response){
         try {
             // Adjust the x-coordinate to be centered
-            p.goal = std::make_tuple(request->x, request->y, request->z);
+            p.goal = std::make_tuple(response->x, response->y, response->z);
             p.valid = true;
 
             RCLCPP_INFO(this->get_logger(), "Target Pose: x=%f, y=%f, z=%f", std::get<0>(p.goal), std::get<1>(p.goal), std::get<2>(p.goal));
-            RCLCPP_INFO(this->get_logger(), "Request Pose: x=%f, y=%f, z=%f, w=%f", request->x, request->y, request->z, request->w);
 
             // Update the robot's velocity command
             if (p.valid) {
-                // RCLCPP_INFO(this->get_logger(), "I DETECT HUMAN");
                 GettingHuman();
             } else {
-                // RCLCPP_ERROR(this->get_logger(), "I LOST HUMAN");
                 LostHuman();
             }
-
-            response->success = true;
         } catch (const std::exception &e) {
             p.valid = false;
             RCLCPP_ERROR(this->get_logger(), "Failed to get target pose: %s", e.what());
             LostHuman();
-            response->success = false;
         }
     }
 
@@ -70,7 +91,7 @@ private:
         double person_x = std::get<0>(p.goal);
         double person_y = std::get<1>(p.goal);
 
-        // Robot's rotation control: use x value to align to the center of the camera
+        // Robot's rotation control: use y value to align to the center of the camera
         if (person_y < -0.05)
             velOutput.angular.z = R_VEL;
         else if (person_y > 0.05)
@@ -78,7 +99,7 @@ private:
         else
             velOutput.angular.z = 0;
 
-        // Robot's forward/backward control: use z value to maintain a certain distance
+        // Robot's forward/backward control: use x value to maintain a certain distance
         if (person_x > 0.5) {
             RCLCPP_INFO(this->get_logger(), "FORWARD");
             velOutput.linear.x = F_VEL;
@@ -90,18 +111,19 @@ private:
             velOutput.linear.x = 0;
         }
 
-        publisher_->publish(velOutput);
+        pub->publish(velOutput);
     }
 
     void LostHuman() {
         RCLCPP_ERROR(this->get_logger(), "I LOST HUMAN");
         velOutput.linear.x = 0;
         velOutput.angular.z = 0;
-        publisher_->publish(velOutput);
+        pub->publish(velOutput);
     }
 
-    rclcpp::Service<yolov8_msgs::srv::TargetPose>::SharedPtr service_server_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Client<yolov8_msgs::srv::TargetPose>::SharedPtr srv_client;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char **argv) {
